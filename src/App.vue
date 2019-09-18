a0973be8598a0757847bffedb7eab50d1e00d4f8
heshijia <1634943446@qq.com>
2019-09-18 19:00:32 +0800

    初始化测试

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button-test></button-test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import ButtonTest from './components/button-test/button-test'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      ButtonTest
+    }
   }
-}
 </script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+    margin-top 60px
 </style>
---
+++ src/components/button-test/button-test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="btn-wrap">
+    <cube-button type="submit" icon="cubeic-right">Button</cube-button>
+    <cube-button :light="true">Light Button</cube-button>
+    <cube-button :inline="true">Inline Button</cube-button>
+    <cube-button :outline="true">Outline Button</cube-button>
+    <cube-button :primary="true">Primary Button</cube-button>
+    <div class="tab">
+      <ul>
+        <li>标题一</li>
+        <li>标题二</li>
+        <li>标题三</li>
+      </ul>
+    </div>
+    <cube-button @click="$refs.tip.show()">Show tip</cube-button>
+    <cube-tip ref="tip" direction="bottom">Tip</cube-tip>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'button-test'
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~@common/stylus/variable"
+  .tab
+    width: 100%;
+    height: 36px;
+    ul
+      display flex
+      justify-content space-around
+      background-color $color-blue
+      li
+        font-size $fontsize-medium
+        color: $color-col-line
+        text-align center
+        line-height: 36px
+        background-color: $color-background-ss
+        padding 0 12px
+</style>
